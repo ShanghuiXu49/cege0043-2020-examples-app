f0f2f6e966f2d028dae2acc4f522656ba978ce4f
Claire Ellul <c.ellul@ucl.ac.uk>
2020-02-16 15:12:07 -0500

    working pwa example

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,4 +1,4 @@
-const staticAssets = [
+const cacheFiles = [
     './',
     './index.html',
     './serviceWorker.js',
@@ -6,115 +6,100 @@ const staticAssets = [
     './launcher-icon-144.png'
 ];
 
-
-//https://developers.google.com/web/fundamentals/instant-and-offline/offline-cookbook
-// install all the static assets 
-//CSS, images, fonts, JS, templates… basically anything you'd consider static to that "version" of your site.
-//These are things that would make your site entirely non-functional if they failed to fetch, things an equivalent native-app would make part of the initial download.
-// this 
-// (NOT USED HERE - you can wait until the app has installed and add
-//furthe relements afterwards - e.g. items for later levels in the game
-// that way the actual install process isn't too long)
-
-/*self.addEventListener('install', async event => {
-    const cache = await caches.open('static-cache');
-    cache.addAll(staticAssets);
-});*/
+var cacheName = 'v7'; 
 
 
-// code adapted from here: https://www.youtube.com/watch?v=87RU7v6Y-bk accessed 28th Dec 2019
-self.addEventListener('install',function(event) {
-    event.waitUntil(
-        caches.open('static-cache')
-        .then (cache => {
-            return cache.addAll(staticAssets);
+
+self.addEventListener('install', function(e) {
+    console.log('[ServiceWorker] Installed');
+
+    // e.waitUntil Delays the event until the Promise is resolved
+    e.waitUntil(
+
+        // Open the cache
+        caches.open(cacheName).then(function(cache) {
+
+            // Add all the default files to the cache
+            console.log('[ServiceWorker] Caching cacheFiles');
+            return cache.addAll(cacheFiles);
         })
-    );
+    ); // end e.waitUntil
 });
 
 
-/*self.addEventListener('activate', event => {
-  const currentCaches = ['static-cache'];
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return cacheNames.filter(cacheName => !currentCaches.includes(cacheName));
-    }).then(cachesToDelete => {
-      return Promise.all(cachesToDelete.map(cacheToDelete => {
-        return caches.delete(cacheToDelete);
-      }));
-    }).then(() => self.clients.claim())
-  );
-});*/
+self.addEventListener('activate', function(e) {
+    console.log('[ServiceWorker] Activated');
 
-self.addEventListener('fetch', event=>{
-    event.respondWith(
-        caches.match(event.request)
-        .then (response => {
-            return response || fetch(event.request);
-        }
-    )
-    );
-}); 
+    e.waitUntil(
 
-/*self.addEventListener('fetch', event => {
-  if (event.request.method === 'GET') {
-    let url = event.request.url.indexOf(self.location.origin) !== -1 ?
-      event.request.url.split(`${self.location.origin}/`)[1] :
-      event.request.url;
-    let isFileCached = $FILES.indexOf(url) !== -1;
+        // Get all the cache keys (cacheName)
+        caches.keys().then(function(cacheNames) {
+            return Promise.all(cacheNames.map(function(thisCacheName) {
 
-    if (isFileCached) {
-      event.respondWith(
-        caches.open($CACHE_STORE)
-          .then(cache => {
-            return cache.match(url)
-              .then(response => {
-                if (response) {
-                  return response;
+                // If a cached item is saved under a previous cacheName
+                if (thisCacheName !== cacheName) {
+
+                    // Delete that cached file
+                    console.log('[ServiceWorker] Removing Cached Files from Cache - ', thisCacheName);
+                    return caches.delete(thisCacheName);
                 }
-                throw Error('There is not response for such request', url);
-              });
-          })
-          .catch(error => {
-            return fetch(event.request);
-          })
-      );
-    }
-  }
-});
-*/
-// with a service worker, every request for a URL is passed to the 
-// worker instead of being made directly to the server
-// that way offline situations can be handled more easily
-self.addEventListener('fetch', event => {
-    const req = event.request;
-    const url = new URL(req.url);
+            }));
+        })
+    ); // end e.waitUntil
 
-    // if the request orginates from the same url as the app then 
-    // check cache first
-    // nb - note that location.url = self.location.url = window.location.url
-    // can reverse this situation to check the network first 
-    // and only use the local cache if the network is not available
-    if (url.origin === location.url) {
-        event.respondWith(cacheFirst(req));
-    } else {
-        event.respondWith(networkFirst(req));
-    }
 });
 
-async function cacheFirst(req) {
-    const cachedResponse = caches.match(req);
-    return cachedResponse || fetch(req);
-}
 
-async function networkFirst(req) {
-    const cache = await caches.open('dynamic-cache');
+self.addEventListener('fetch', function(e) {
+    console.log('[ServiceWorker] Fetch', e.request.url);
 
-    try {
-        const res = await fetch(req);
-        cache.put(req, res.clone());
-        return res;
-    } catch (error) {
-        return await cache.match(req);
-    }
-}
+    // e.respondWidth Responds to the fetch event
+    e.respondWith(
+
+        // Check in cache for the request being made
+        caches.match(e.request)
+
+
+            .then(function(response) {
+
+                // If the request is in the cache
+                if ( response ) {
+                    console.log("[ServiceWorker] Found in Cache", e.request.url, response);
+                    // Return the cached version
+                    return response;
+                }
+
+                // If the request is NOT in the cache, fetch and cache
+
+                var requestClone = e.request.clone();
+                fetch(requestClone)
+                    .then(function(response) {
+
+                        if ( !response ) {
+                            console.log("[ServiceWorker] No response from fetch ")
+                            return response;
+                        }
+
+                        var responseClone = response.clone();
+
+                        //  Open the cache
+                        caches.open(cacheName).then(function(cache) {
+
+                            // Put the fetched response in the cache
+                            cache.put(e.request, responseClone);
+                            console.log('[ServiceWorker] New Data Cached', e.request.url);
+
+                            // Return the response
+                            return response;
+
+                        }); // end caches.open
+
+                    })
+                    .catch(function(err) {
+                        console.log('[ServiceWorker] Error Fetching & Caching New Data', err);
+                    });
+
+
+            }) // end caches.match(e.request)
+    ); // end e.respondWith
+});
